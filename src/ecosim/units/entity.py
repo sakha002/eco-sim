bb71899bc02c4ed151d804818c520e882b29ddb4
sakhavanhejazi <h.akhavanhejazi@shell.com>
2021-03-04 22:50:20 -0800

    adding simtime

---
+++ src/ecosim/units/entity.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+from dataclasses import dataclass
+from uuid import uuid4
+
+
+
+@dataclass
+class Person:
+    name: str
+    uid
+    worth
+    function
+    ownerships
+    partnerships
+    services
+
+
+
+
+
